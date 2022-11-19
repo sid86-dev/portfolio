c22f8bae3769c7a014523c2401484100dbe58d35
Siddhartha Roy <sid86harth@gmail.com>
2022-11-20 02:44:40 +0530

    Handle theme on frontend

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,119 +1,113 @@
-import React, { useContext, useState } from "react";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import WorkIcon from "@mui/icons-material/Work";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import TerminalIcon from "@mui/icons-material/Terminal";
-import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
-import { Context } from "../context/store";
-import { AppStoreContext } from "../types";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import Image from "next/image";
+import React, { useEffect, useState } from 'react';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import WorkIcon from '@mui/icons-material/Work';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 function Navbar() {
-  const [state, setState] = useContext<AppStoreContext>(Context);
-  const [isActive, setIsActive] = useState(false);
+	const [isActive, setIsActive] = useState(false);
+	const { theme, setTheme } = useTheme();
+	const [renderTheme, setRenderTheme] = useState<string | undefined>('dark');
 
-  const style = {
-    navbar: `navbar navbar-expand-lg py-3 ${
-      state.isDark ? "bg-dark-sm" : "bg-light"
-    }`,
-    navLinks: `nav-link active ${
-      !state.isDark
-        ? "text-mid-dark nav-link-dark"
-        : "nav-link-light text-light"
-    }`,
-  };
+	useEffect(() => {
+		setRenderTheme(theme);
+	}, [theme]);
 
-  return (
-    <nav className={style.navbar}>
-      <div className="container-fluid px-5">
-        <Link className="navbar-brand" href="/">
-          <Image
-            priority
-            alt=""
-            src="/images/logo/dark.png"
-            height={50}
-            width={50}
-            className="bg-light bg-animate rounded-5 p-1 pointer"
-          />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mx-auto mb-2 mb-lg-0">
-            <li className="nav-item mx-lg-3">
-              <a className={style.navLinks} aria-current="page" href="#">
-                <h6 className="fs-6">
-                  <WorkIcon className="navicon" /> Projects
-                </h6>
-              </a>
-            </li>
-            <li className="nav-item mx-lg-3">
-              <a
-                className={style.navLinks}
-                href={"https://github.com/sid86-dev"}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <h6 className="fs-6">
-                  <GitHubIcon /> Github
-                </h6>
-              </a>
-            </li>
-            <li className="nav-item mx-lg-3">
-              <a className={style.navLinks}>
-                <Link href={"/meet"}>
-                  <h6 className="fs-6 pointer">
-                    <MeetingRoomIcon /> Meet
-                  </h6>
-                </Link>
-              </a>
-            </li>
-          </ul>
-          <div className="d-flex">
-            <button
-              className="btn btn-dark mx-lg-3"
-              type="submit"
-              onClick={() => {
-                // @ts-ignore
-                setState((prev) => ({
-                  ...prev,
-                  ["isDark"]: !state.isDark,
-                }));
-                setIsActive(!isActive);
-              }}
-            >
-              <motion.div
-                animate={{
-                  rotate: isActive ? 180 : 0,
-                }}
-              >
-                {state.isDark ? (
-                  <Brightness7Icon className="mb-1" />
-                ) : (
-                  <Brightness4Icon />
-                )}
-              </motion.div>
-            </button>
-            <button className="btn btn-dark mx-2" type="submit">
-              <TerminalIcon />
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+	const style = {
+		navbar: 'navbar navbar-expand-lg py-3',
+		navLinks: 'nav-link active',
+	};
+
+	return (
+		<nav className={style.navbar}>
+			<div className='container-fluid px-5'>
+				<Link className='navbar-brand' href='/'>
+					<Image
+						priority
+						alt=''
+						src='/images/logo/dark.png'
+						height={50}
+						width={50}
+						className='bg-light bg-animate rounded-5 p-1 pointer'
+					/>
+				</Link>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon'></span>
+				</button>
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav me-auto mx-auto mb-2 mb-lg-0'>
+						<li className='nav-item mx-lg-3'>
+							<a className={style.navLinks} aria-current='page' href='#'>
+								<h6 className='fs-6'>
+									<WorkIcon className='navicon' /> Projects
+								</h6>
+							</a>
+						</li>
+						<li className='nav-item mx-lg-3'>
+							<a
+								className={style.navLinks}
+								href={'https://github.com/sid86-dev'}
+								target='_blank'
+								rel='noreferrer'
+							>
+								<h6 className='fs-6'>
+									<GitHubIcon /> Github
+								</h6>
+							</a>
+						</li>
+						<li className='nav-item mx-lg-3'>
+							<a className={style.navLinks}>
+								<Link href={'/meet'}>
+									<h6 className='fs-6 pointer'>
+										<MeetingRoomIcon /> Meet
+									</h6>
+								</Link>
+							</a>
+						</li>
+					</ul>
+					<div className='d-flex'>
+						<button
+							className='btn btn-dark mx-lg-3'
+							type='submit'
+							onClick={() => {
+								setTheme(theme === 'dark' ? 'light' : 'dark');
+								setIsActive(!isActive);
+							}}
+						>
+							<motion.div
+								animate={{
+									rotate: isActive ? 180 : 0,
+								}}
+							>
+								{renderTheme === 'dark' ? (
+									<Brightness7Icon className='mb-1' />
+								) : renderTheme === 'light' ? (
+									<Brightness4Icon />
+								) : null}
+							</motion.div>
+						</button>
+						<button className='btn btn-dark mx-2' type='submit'>
+							<TerminalIcon />
+						</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,17 +1,10 @@
-import React, { useContext } from "react";
-import { Context } from "../context/store";
-import { AppStoreContext } from "../types";
+import React from 'react';
 
 type Props = {
-  children: JSX.Element | JSX.Element[] | string | string[];
+	children: JSX.Element | JSX.Element[] | string | string[];
 };
 
 // Theme wrapper over app
 export const Wrapper = ({ children }: Props) => {
-  const [state] = useContext<AppStoreContext>(Context);
-
-  const style = {
-    body: state.isDark ? "bg-dark text-light" : "bg-gray text-mid-dark",
-  };
-  return <div className={style.body}>{children}</div>;
+	return <div className='wrapper'>{children}</div>;
 };
