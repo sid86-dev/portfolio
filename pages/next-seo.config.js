4b2fe77ffdd7bd7bc76904a8a57ada131622d140
Siddhartha Roy <sid86harth@gmail.com>
2022-11-20 05:42:27 +0530

    update eslint config

---
+++ pages/next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+	title: 'Next.js SEO',
+	description: '',
+	openGraph: {
+		type: 'website',
+		locale: 'en_IE',
+		url: 'https://www.example.ie/',
+		site_name: 'Next.js SEO',
+	},
+	twitter: {
+		handle: '@handle',
+		site: '@site',
+		cardType: 'summary_large_image',
+	},
+};
