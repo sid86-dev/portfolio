ce68d7b4a2e71b17d4ae361985e3ad239f24c15d
Siddhartha Roy <sid86harth@gmail.com>
2023-05-08 01:34:46 +0530

    cypress config

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,7 +1,7 @@
 describe('Navigation', () => {
 	it('should navigate to the search page', () => {
 		// Start from the index page
-		cy.visit('https://sid86.me');
+		cy.visit('/');
 
 		// Find a link with an href attribute containing "search" and click it
 		cy.get('a[href*="search"]').click();
@@ -9,8 +9,10 @@ describe('Navigation', () => {
 		// The new url should include "/search"
 		cy.url().should('include', '/search');
 
+		cy.wait(3000);
+
 		// Type query into search bar
-		cy.get('input').type('Offerwall');
+		cy.get('input').type('Offerwall', { delay: 500 });
 
 		// The new page should contain an h1 with "search page"
 		cy.get('p').contains('Now complete task an earn money while having fun');
